09385e0c63b0ba94a4ab8ce8c736ac1b569490a8
Gaurangi97 <nimashaumaya64@gmail.com>
2024-03-24 17:37:24 +0530

    second commit

--- EmailApp.java
+++
@@ -1,6 +0,0 @@
-package Email app;
-public class Email app{
-    public static void main(string[]args){
-        Email em1=new Email("John","Smith");
-    }
-}
---
+++ Emaillapp.java
@@ -0,0 +1,7 @@
+package emailapp;
+public class Email App
+{
+public static void main(String[]args){
+Email em1=new Email("John","smith");
+}
+}
